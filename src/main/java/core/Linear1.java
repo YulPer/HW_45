c475a3a5437bee7c4175f3dd15601d91593d4088
Yul Per <yuliiaperedrii.qa@gmail.com>
2019-08-19 12:13:52 -0700

    Initial commit

---
+++ src/main/java/core/Linear1.java
@@ -0,0 +1,26 @@
+package core;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Linear1 {
+
+	public static void main(String[] args) throws Exception {
+
+		System.setProperty("webdriver.gecko.driver", "./resources/webdrivers/mac/geckodriver.sh");
+		WebDriver driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+		driver.get("http://alex.academy/exe/signup/www/index.php");
+
+		driver.findElement(By.id("id_fname")).sendKeys("John");
+		driver.findElement(By.id("id_lname")).sendKeys("Smith");
+		driver.findElement(By.id("id_email")).sendKeys("abc@gmail.com");
+		driver.findElement(By.id("id_phone")).sendKeys("415 555-1212");
+		driver.findElement(By.id("id_submit_button")).click();
+		System.out.println(driver.getTitle().equals("Confirmation") ? "pass" : "fail");
+		driver.quit();
+	}
+}
